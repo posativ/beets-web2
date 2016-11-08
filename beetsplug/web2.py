2f8387e49db097aa98909cd2226c698829bb333e
Martin Zimmermann <info@posativ.org>
2016-11-08 20:19:52 +0100

    add reverse proxy wsgi thingy

--- beetsplug/web2.py
+++ beetsplug/web2.py
@@ -167,6 +167,22 @@ class Web2(object):
         return HTTPResponse(body, **headers)
 
 
+class SubURI(object):
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        return self.app(environ, start_response)
+
+
 def make_app(lib):
     web2 = Web2(lib)
 
@@ -186,7 +202,7 @@ def make_app(lib):
     app.route('/album/values/<key:path>', 'GET', web2.get_album_unique_field_values)
     app.route('/album/query/<query:path>', 'GET', web2.get_album_query)
 
-    return app
+    return SubURI(app)
 
 
 class Web2Plugin(BeetsPlugin):
